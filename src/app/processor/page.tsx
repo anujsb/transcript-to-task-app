9ce953465f96b0b017f3262accba9cd04bb430eb
anujsb <141896390+anujsb@users.noreply.github.com>
2025-01-15 19:39:55 +0530

    gemini integrated

---
+++ src/app/processor/page.tsx
@@ -0,0 +1,12 @@
+import TranscriptProcessor from "@/components/TranscriptProcessor";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <TranscriptProcessor />
+    </div>
+  );
+};
+
+export default page;
